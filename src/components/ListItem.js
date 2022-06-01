b1a17d3533bca136dafa2a996225a2bfa8df033a
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-06-01 11:07:22 -0400

    Feat: create new components
    Were created 3  different component which are ; Cart.js, Header.js, ListItem.js and those were imported and implemented in the app.js

---
+++ src/components/ListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Cart from './Cart';
+
+const  ListItem = () => {
+  return (
+    <div>
+        <Header/>
+        <div>
+            <Cart/>
+        </div>
+    </div>
+  )
+}
+
+export default ListItem;
