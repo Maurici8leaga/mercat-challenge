dd1b0e5b774d57eadbdd5956e12d9b70362eb08f
Mauricio Oleaga <maoa.develoment@gmail.com>
2022-11-28 14:35:39 -0400

    Fixing 404 refresg bug

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import ListItem from './components/ListItem';
-import Header from './components/Header';
-import Checkout from './components/Checkout';
-import store from './reducers/store';
-
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import ListItem from "./components/ListItem";
+import Header from "./components/Header";
+import Checkout from "./components/Checkout";
+import store from "./reducers/store";
 
 function App() {
-  return (
-    <>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<ListItem />} />
-            <Route path="/checkout" element={<Checkout />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
-    </>
-  );
+	return (
+		<>
+			<Provider store={store}>
+				<BrowserRouter basename="/mercat-challenge">
+					<Header />
+					<Routes>
+						<Route path="/" element={<ListItem />} />
+						<Route path="/checkout" element={<Checkout />} />
+					</Routes>
+				</BrowserRouter>
+			</Provider>
+		</>
+	);
 }
 
 export default App;
